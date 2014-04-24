08c20caf1462d995954eddcc521248f460af4ef4
Francis <lihuiyi0406@gmail.com>
2014-04-24 21:33:31 +0800

    Add Splash screen(startup flash window)

--- SimuTraining/App.xaml.cs
+++ SimuTraining/App.xaml.cs
@@ -4,6 +4,7 @@ using System.Configuration;
 using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Threading;
 using System.Windows;
 
 namespace SimuTraining
@@ -13,5 +14,18 @@ namespace SimuTraining
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            SplashScreen sp = new SplashScreen("res/img/welcome.png");
+            sp.Show(true, true);
+            sp.Close(new TimeSpan(0, 0, 3));
+
+            Window main = new MainWindow();
+            main.ShowActivated = false;
+            Thread.Sleep(3000);
+            main.Show();
+
+            base.OnStartup(e);
+        }
     }
 }
