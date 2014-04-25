ff02040928b36f3743a7226372c7db00313a4a24
Francis <lihuiyi0406@gmail.com>
2014-04-25 15:16:15 +0800

    haven't finished mp4support, go for layout

---
+++ SimuTraining/util/VideoUtil.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimuTraining.util
+{
+    public class VideoUtil
+    {
+        public static void encode()
+        { 
+
+        }
+
+        public static void decode()
+        { 
+            
+        }
+    }
+}
