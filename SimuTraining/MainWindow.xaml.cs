7d5ce71750ebd832a010897d8e0a7f1a09e417b4
Francis <lihuiyi0406@gmail.com>
2014-04-25 16:53:43 +0800

    Finish basic layout, trying to add breadcrumb.

--- SimuTraining/MainWindow.xaml.cs
+++ SimuTraining/MainWindow.xaml.cs
@@ -23,6 +23,73 @@ namespace SimuTraining
         public MainWindow()
         {
             InitializeComponent();
+
+            initMainPage();
+        }
+
+        private void mainPage_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void return_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void initMainPage()
+        {
+            // first remove previous children
+            body.Children.RemoveRange(0, body.Children.Count);
+
+            // add main page material
+            StackPanel sp = new StackPanel() { Orientation = Orientation.Horizontal, VerticalAlignment = VerticalAlignment.Center, HorizontalAlignment = HorizontalAlignment.Center };
+
+            Image b1 = new Image() { Margin = new Thickness(100) };
+            b1.Source = new BitmapImage(new Uri("res/img/main_1.png", UriKind.Relative));
+            b1.Width = 128;
+            b1.Height = 128;
+            b1.MouseLeftButtonDown += new MouseButtonEventHandler(enterMainList);
+
+            Image b2 = new Image() { Margin = new Thickness(100) };
+            b2.Source = new BitmapImage(new Uri("res/img/main_2.png", UriKind.Relative));
+            b2.Width = 128;
+            b2.Height = 128;
+
+            Image b3 = new Image() { Margin = new Thickness(100) };
+            b3.Source = new BitmapImage(new Uri("res/img/main_3.png", UriKind.Relative));
+            b3.Width = 128;
+            b3.Height = 128;
+
+            sp.Children.Add(b1);
+            sp.Children.Add(b2);
+            sp.Children.Add(b3);
+
+            body.Children.Add(sp);
+        }
+
+        private void initList()
+        {
+            // first remove previous children
+            body.Children.RemoveRange(0, body.Children.Count);
+
+            // add main page material
+            StackPanel sp = new StackPanel() { Orientation = Orientation.Horizontal, VerticalAlignment = VerticalAlignment.Center, HorizontalAlignment = HorizontalAlignment.Center };
+
+            Image b1 = new Image() { Margin = new Thickness(100) };
+            b1.Source = new BitmapImage(new Uri("res/img/main_1.png", UriKind.Relative));
+            b1.Width = 128;
+            b1.Height = 128;
+            b1.MouseLeftButtonDown += new MouseButtonEventHandler(enterMainList);
+
+            sp.Children.Add(b1);
+
+            body.Children.Add(sp);
+        }
+
+        private void enterMainList(object sender, MouseButtonEventArgs e)
+        {
+            initList();
         }
     }
 }
