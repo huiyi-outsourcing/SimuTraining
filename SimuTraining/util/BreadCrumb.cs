f9533c12803fe9145ff6f776fe51492b05947d9f
Francis <lihuiyi0406@gmail.com>
2014-04-26 15:53:10 +0800

    finish Directory.xml, added all directory info. Create BreadCrumb.cs class for BreadCrumb support. Add TrainingWindow for ui.

---
+++ SimuTraining/util/BreadCrumb.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace SimuTraining.util
+{
+    public class BreadCrumb
+    {
+        private static Node root = null;
+
+        private BreadCrumb() { }
+
+        public static Node getRoot()
+        {
+            if (root == null)
+            {
+                root = new Node();
+                root.Name = "首页";
+                root.Parent = null;
+
+                // get all children
+            }
+            return root;
+        }
+    }
+
+    public class Node
+    {
+        private String name;
+        public String Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        private Node parent;
+        public Node Parent
+        {
+            get { return parent; }
+            set { parent = value; }
+        }
+
+        private List<Node> children;
+        public List<Node> Children
+        {
+            get { return children; }
+            set { children = value; }
+        }
+
+        public Node() { }
+
+        public Node(String name, Node parent, List<Node> children) 
+        {
+            this.name = name;
+            this.parent = parent;
+            this.children = children;
+        }
+    }
+}
